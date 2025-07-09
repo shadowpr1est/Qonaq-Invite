e14ee706aafff661e67b5e3dad489d84368f8250
shadowpr1est <alisher.arginbekov@mail.ru>
2025-07-09 13:19:10 +0500

    ...

--- backend/src/dependencies/__init__.py
+++ backend/src/dependencies/__init__.py
@@ -1,1 +1,1 @@
-from .auth import get_current_user, get_current_active_user, get_optional_current_user
+from .auth import get_current_user, get_current_active_user, get_user_from_request, require_admin_user
